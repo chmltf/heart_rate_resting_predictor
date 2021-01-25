a2f9c914f6956f07a48b22c67564ea1857f87a28
Lauren Fleming <L.T.Fleming@leeds.ac.uk>
2021-01-25 16:04:33 +0000

    Lin reg model added

--- read_visualize_fitbit_data.py
+++ read_visualize_fitbit_data.py
@@ -33,3 +33,48 @@ for exercise_file in glob.glob(os.path.join(os.getcwd(), 'LaurenFleming_original
 print(heart_rate_data.head())
 print(sleep_data.head())
 print(exercise_data.head())
+
+start = datetime.time(1,0,0)
+end = datetime.time(5,30,0)
+
+heart_rate_data.columns = ['dateTime', 'bpm']
+heart_rate_data['dateTime'] = pd.DatetimeIndex(heart_rate_data['dateTime'])
+heart_rate_data.set_index(keys = 'dateTime', inplace = True)
+rhrData = heart_rate_data.between_time(start, end) #Return DataFrame with data between 1 AM and 5:30 AM, average of which will be RHR
+rhrData.reset_index(inplace = True)
+rhrData.columns = ['dateTime', 'bpm']
+rhrData['date'] = rhrData.loc[:,'dateTime'].dt.date
+rhr_df = pd.pivot_table(rhrData, values = 'bpm', index = 'date', aggfunc = np.mean)
+
+exercise_data['date'] = exercise_data['startTime'].dt.date
+exercise_data_df = pd.pivot_table(exercise_data, values = 'duration', columns = 'activityName', index = 'date', aggfunc = np.sum)
+exercise_data_df.drop(['Run', 'Outdoor Bike'], axis = 1, inplace = True)   #Run and outdoor bike activities are completely null
+exercise_data_df = exercise_data_df.fillna(0)
+exercise_data_df['workout'] = exercise_data_df['Aerobic Workout'] + exercise_data_df['Sport']  #Working out can register on Fitbit as either of these
+exercise_data_df.drop(['Aerobic Workout', 'Sport'], axis = 1, inplace = True) #Don't need this now, just clean up step
+
+sleep_data.set_index('dateOfSleep', inplace = True)
+
+all_data = exercise_data_df.join([sleep_data, rhr_df])
+all_data.reset_index(inplace = True)
+all_data.drop('index', axis = 1, inplace = True)
+index_list = list(range(0, round(len(all_data))))
+np.random.shuffle(index_list)
+trainIndex = index_list[0:(round(0.8*len(index_list)))]
+crossValIndex = index_list[len(trainIndex): round((((len(index_list)-len(trainIndex))/2) + len(trainIndex)))]
+testIndex = index_list[(len(train_index) + len(crossValIndex)) :]
+
+X = all_data.iloc[:,:-1]
+Xtrain = X.loc[trainIndex, :]
+Xtrain = Xtrain.to_numpy()
+Xtrain = np.c_[np.ones((Xtrain.shape[0],1)), Xtrain]
+y = all_data.loc[:,'bpm'].to_numpy()
+ytrain = y[trainIndex]
+
+XcrossVal = X.loc[crossValIndex, :].to_numpy()
+ycrossVal = y[crossValIndex]
+test = X.loc[testIndex, :].to_numpy()
+ytest = y[testIndex]
+
+theta_best = np.linalg.inv(Xtrain.T.dot(Xtrain)).dot(Xtrain.T).dot(ytrain) #use normal equation to find theta
+y_predict = XcrossVal * theta_best
